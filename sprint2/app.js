9c8ab1ce877862d26936db124c27f46931422964
brianlopez93 <brianrrhh93@gmail.com>
2021-07-26 20:56:38 -0300

    empiezo a trabajar en el carrito

--- sprint2/app.js
+++ sprint2/app.js
@@ -5,14 +5,11 @@ const path = require('path');
 
 app.use(express.static('public'));
 
-app.get('/',(req,res) => res.sendFile(path.join(__dirname, '/views/index.html')));
-app.get('/product',(req,res) => res.sendFile(path.join(__dirname, '/views/productDetail.html')));
-app.get('/cart',(req,res) => res.sendFile(path.join(__dirname, '/views/productCart.html')));
-app.get('/login',(req,res) => res.sendFile(path.join(__dirname, '/views/login.html')));
-app.get('/register',(req,res) => res.sendFile(path.join(__dirname, '/views/register.html')));
-
-
-
-
+app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views','index.html')));
+app.get('/product',(req,res) => res.sendFile(path.join(__dirname, 'views','productDetail.html')));
+app.get('/cart',(req,res) => res.sendFile(path.join(__dirname, 'views','productCart.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname, 'views','login.html')));
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname, 'views','register.html')));
+app.get('/footer',(req,res) => res.sendFile(path.join(__dirname, 'views','footer.html')));
 
 app.listen(port, () => console.log('Servidor en el puerto ' + port));
