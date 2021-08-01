271da81d4d5a7e1ee714c6d79d27c0d365ee19f6
JuanPabloMingorance <mingorancejuan5@gmail.com>
2021-08-01 20:26:32 -0300

    Resuelve conflictos

--- sprint2/app.js
+++ sprint2/app.js
@@ -7,7 +7,6 @@ app.use(express.static('public'));
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views','index.html')));
 app.get('/product',(req,res) => res.sendFile(path.join(__dirname, 'views','productDetail.html')));
-<<<<<<< HEAD
 app.get('/cart',(req,res) => res.sendFile(path.join(__dirname, 'views','cart.html')));
 app.get('/login',(req,res) => res.sendFile(path.join(__dirname, 'views','login.html')));
 app.get('/register',(req,res) => res.sendFile(path.join(__dirname, 'views','register.html')));
@@ -15,14 +14,5 @@ app.get('/footer',(req,res) => res.sendFile(path.join(__dirname, 'views','footer
 app.get('/cartEntrega',(req,res) => res.sendFile(path.join(__dirname, 'views','cartEntrega.html')));
 app.get('/cartPago',(req,res) => res.sendFile(path.join(__dirname, 'views','cartPago.html')));
 app.get('/cartFinal',(req,res) => res.sendFile(path.join(__dirname, 'views','cartFinal.html')));
-=======
-app.get('/cart',(req,res) => res.sendFile(path.join(__dirname, 'views','productCart.html')));
-app.get('/login',(req,res) => res.sendFile(path.join(__dirname, 'views','login.html')));
-app.get('/register',(req,res) => res.sendFile(path.join(__dirname, 'views','register.html')));
-
-
-
-
->>>>>>> vistaProduct/Juan
 
 app.listen(port, () => console.log('Servidor en el puerto ' + port));
