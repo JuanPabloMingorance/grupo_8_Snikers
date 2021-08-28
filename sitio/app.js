21af3fb49acd24029bd45c0856b45bd8e41f7f37
JuanPabloMingorance <mingorancejuan5@gmail.com>
2021-08-27 23:58:54 -0300

    Modifica vista login

--- sitio/app.js
+++ sitio/app.js
@@ -23,8 +23,8 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(methodOverride('_method'))
 
 app.use('/', mainRouter);
-app.use('/users', usersRouter);
-app.use('/products', productsRouter);
+app.use('/users',usersRouter);
+app.use('/products',productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
