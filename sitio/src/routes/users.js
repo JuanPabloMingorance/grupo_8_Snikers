18c0d59ec6ae244195f8d1d7002dcc04ac32d722
lumilamorelli <lumilamorelli@gmail.com>
2021-09-21 22:16:34 -0300

    cambios en users.js

--- sitio/src/routes/users.js
+++ sitio/src/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {register, login,processLogin,processRegister,profile} = require('../controllers/usersController');
 const loginValidator = require('../validations/loginValidator');
-const registeValidator = require('../validations/registerValidator');
+const registerValidator = require('../validations/registerValidator');
 
 router.get('/register', register);
 router.post('/register',registerValidator,processRegister);
@@ -12,25 +12,4 @@ router.get('/profile',profile);
 
 module.exports = router;
 
-const express = require('express');
-const router = express.Router();
 
-const {register, processRegister,login,processLogin,profile, logout,update} = require('../controllers/usersController')
-const registerValidator = require('../validations/registerValidator');
-const loginValidator = require('../validations/loginValidator');
-
-
-const profileValidator = require('../validations/profileValidator');
-
-/* /users */
-router.get('/register',register);
-router.post('/register',registerValidator, processRegister);
-router.get('/login',login);
-router.post('/login',loginValidator, processLogin);
-
-router.get('/profile',profile);
-
-router.get('/logout',logout);
-
-
-module.exports = router;
