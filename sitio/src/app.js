7339dfe727c86d4f903b0666150eb72e9ece7b8a
brianlopez93 <brianrrhh93@gmail.com>
2021-10-22 20:21:12 -0300

    agrega sequelize

--- sitio/src/app.js
+++ sitio/src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -19,14 +20,14 @@ app.use(methodOverride('_method'));
 
 
 // view engine setup
-app.set('views', path.join(__dirname,'./src','views'));
+app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'../public')));
 
 app.use(session(({
   secret: 'Snikers',
